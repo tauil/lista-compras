a80259a1b94e6d92940a0b1b16e4aca2cccd8a04
Rafael B. Tauil <rafael@tauil.com.br>
2010-12-22 15:46:08 -0200

    First commit with generated rails files and rvm config file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListaCompras::Application.config.secret_token = 'e1594f1c85623f97546cfcefc48a62aa5abe0a1200cfb22c3d63bfe55442847dc2561f9637aee824a6b42a34d879f8bafcc535b2373de4bcdfd1e05d5ee9dcad'
