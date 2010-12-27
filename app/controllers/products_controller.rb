343715596adb33ee3cc775629a4c7fdd3aa12d55
Rafael B. Tauil <rafael@tauil.com.br>
2010-12-27 11:47:15 -0200

    Creating products via AJAX

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,13 @@ class ProductsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @product = @list.products.build(params[:product])
-    flash[:alert] = 'Failed adding product to the list' unless @product.save
-    
-    redirect_to @list
+
+    flash[:alert] = 'Fail to receive the product. Double check the fields.' unless @product.save
+
+    respond_to do |format|
+      format.html { redirect_to @list }
+      format.js
+    end
   end
 
 end
